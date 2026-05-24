a41f8088db097b93f938f8cc1b951b37c4e613a6
Rakel <raalram@gmail.com>
2026-05-24 19:30:02 +0200

    Modificación escena intro. Completando ReadMe

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ Esta versión está preparada como prototipo mínimo viable para clase:
 - `index.html` importa el CSS y el JavaScript modular.
 - `css/style.css` contiene la parte visual de la página.
 - `src/main.js` configura Phaser.
-- `src/scenes/GameScene.js` carga el mapa de Tiled y el personaje.
+- `src/scenes` contiene la diversas escenas del juego: 
+        - DifficultyScene: permite seleccionar la dificultad del juego
+        - MenuScene: escena con el menú del juego
+        - GameScene: contiene toda la lógica del juego
+        - WinScene: escena de Victoria
+        - EndGame: escena de derrota.
 - `src/scenes/EndScene.js` carga la escena final del juego
 - `src/scenes/MenuScene.js` carga la esceba de inicio del juego con el botón jugar
-- `assets/maps/mapa.json` contiene el tileset incrustado para evitar el fallo del `.tsx` externo.
+- `assets` contiene los mapas, archivos de audio, sonidos y sprites del juego
 - `assets/sprites` contiene los sprites de las llaves, enemigos, puerta y el spritecheet del jugador
 
 ## Cómo abrirlo
@@ -25,10 +30,12 @@ No lo abras con doble clic. Usa Live Server / Live Preview en Visual Studio Code
 - Flecha arriba: saltar.
 - Tecla X: disparar
 
-## Qué se corrigió
-
-- Rutas de mapa y sprites más simples: todo está dentro de la misma carpeta del proyecto.
-- Mapa JSON modificado para que Phaser lea el tileset sin depender de `rock_packed.tsx`.
-- Estructura modular limpia.
-- Spawn automático del personaje en una zona segura del mapa.
-- Movimiento personaje y salto y se crearon animaciones
+## Qué trabajo realizó cada componente del grupo:
+- ## Raquel Álvarez Ramos: 
+    Creación del GDD. Implementación del player con mecánicas de movimiento, salto y disparo. Creación de power ups de vida, sistema de puntuación por enemigo eliminado y creación de contador de tiempo. Efectos de sonido de disparo y recolección de vida. Pulido de las escenas de "Win" y "GameOver" añadiendo puntuación total y tiempo así como creación de texto indicativo de falta de llaves cuando el jugador interacciona con la puerta y no tiene las tres llaves necesarias.
+- ## Victor Gago Paz: 
+    Creación de interfaz de usuario con contadores de llaves y vidas y de la mecánica de interacción jugador-puerta. Inicio creación escenas de "Win" y "Game Over"
+- ## Carlos Pablos Rivero: 
+    Implementación de los enemigos terrestres que se mueven dentro de una plataforma y si ven al jugador van a por él y los fantasmas que atraviesan paredes y se mantienen moviéndose dentro de una zona y cuando ven al jugador también le persiguen. Efectos de sonido de daño de enemigos. Implementación de música. Creación de escena de selección del nivel de dificultad del juego.
+- ## Juan Carlos Jibaja 
+    Creación de mapa usando Tiles
--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -1,54 +1,55 @@
-export default class IntroScene extends Phaser.Scene {
+﻿export default class IntroScene extends Phaser.Scene {
   constructor() {
     super({ key: 'IntroScene' });
   }
 
-      init(data) {
+  init(data = {}) {
     this.dificultadSeleccionada = data.dificultad || 'normal';
   }
+
   create() {
-    const centroX = this.cameras.main.width / 2;
+    const centroX = this.scale.width / 2;
+    const centroY = this.scale.height / 2;
 
-
-
-    this.add.text(centroX, 50, 'Biolab Escape', { 
-      fontSize: '40px', 
+    this.add.text(centroX, centroY - 220, 'Biolab Escape', {
+      fontSize: '40px',
       color: '#6cff7c',
       fontFamily: 'Arial',
-      fontStyle: 'bold'
+      fontStyle: 'bold',
+      align: 'center'
     }).setOrigin(0.5);
 
-    // 2. Condensamos las líneas para que no ocupe tanto espacio vertical
-    const textoHistoria = 
-    "Eres un curioso explorador que se ha visto atrapado\n" +
-    "en un laboratorio secreto.\n\n" +
-    "Tu objetivo es abrirte paso entre los monstruos del complejo,\n" +
-    "consiguiendo puntuación al derrotarlos y escapar.\n\n" +
-    "CONTROLES:\n" +
-    "Moverse: Flechas del teclado\n" +
-    "Saltar: Flecha Arriba\n" +
-    "Disparar: Tecla X\n\n" +
-    "Encuentra las 3 llaves de seguridad y llega a la puerta final.";
+    const textoHistoria =
+      "Laboratorio de maxima seguridad\n" +
+      "Eres Exp-00, una criatura experimental\n\n" +
+      "Gracias a un fallo de seguridad has logrado escapar\n" +
+      "colandote por el sistema interno de alcantarillas...\n" +
+      "lograras escapar del laberinto enfrentandote a las criaturas que lo habitan?\n\n" +
+      "CONTROLES:\n" +
+      "Moverse: Flechas del teclado\n" +
+      "Saltar: Flecha Arriba\n" +
+      "Disparar: Tecla X\n\n" +
+      "Encuentra las 3 llaves de seguridad y llega a la puerta final.";
 
-    // 3. Colocamos el texto en la posición Y: 250 (justo en el medio)
-    this.add.text(centroX, 250, textoHistoria, {
+    this.add.text(centroX, centroY, textoHistoria, {
       fontFamily: 'Arial',
       fontSize: '18px',
       color: '#ffffff',
       align: 'center',
-      lineSpacing: 10 
+      lineSpacing: 10,
+      wordWrap: { width: this.scale.width - 80 }
     }).setOrigin(0.5);
 
-    // 4. Bajamos el botón a la posición Y: 460
-    const btnContinuar = this.add.text(centroX, 460, 'INICIAR ESCAPE', {
+    const btnContinuar = this.add.text(centroX, centroY + 220, 'INICIAR ESCAPE', {
       fontSize: '28px',
       color: '#ffffff',
       backgroundColor: '#1f5f3a',
-      padding: { x: 20, y: 10 }
+      padding: { x: 20, y: 10 },
+      align: 'center'
     }).setOrigin(0.5).setInteractive();
 
     btnContinuar.once('pointerdown', () => {
-      this.scene.start('GameScene' , { dificultad: this.dificultadSeleccionada });
+      this.scene.start('GameScene', { dificultad: this.dificultadSeleccionada });
     });
   }
 }
